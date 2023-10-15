267ea951da767a465a59c135ace0a37b0fc13f69
Filipe Campos <filipepcampos24@gmail.com>
2023-10-15 16:53:47 +0100

    feat: now the bot can stop

--- ws/install/robot_controller/share/robot_controller/robot_controller/movement.py
+++ ws/install/robot_controller/share/robot_controller/robot_controller/movement.py
@@ -2,9 +2,12 @@ import math
 
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose2D
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 import numpy as np
+
+from helper import foo
 
 
 def rads_to_deg(rads: float) -> float:
@@ -33,6 +36,30 @@ def get_closest_point(msg: LaserScan) -> LaserPoint:
     ranges = np.array(msg.ranges)
     closest_point_index = np.argmin(ranges)
     return create_laser_point(closest_point_index, msg)
+
+def get_average_point(msg: LaserScan) -> LaserPoint:
+    closest_point = get_closest_point(msg)
+    forward_point_index = int(len(msg.ranges)/2)
+    if msg.ranges[forward_point_index] < 999999 and msg.ranges[forward_point_index] < closest_point.distance + 1.0:
+        return LaserPoint(
+            index=closest_point.index,
+            distance=closest_point.distance,
+            angle=(closest_point.angle)/2,
+        )
+    return closest_point
+
+
+def get_closest_lateral_point(
+    msg: LaserScan, closest_point: LaserPoint
+) -> (LaserPoint, LaserPoint):
+    closest_lateral_sign = 1 if closest_point.angle > 0 else -1
+    lateral_angle = closest_lateral_sign * math.pi / 2
+    lateral_index = int((lateral_angle - msg.angle_min) / msg.angle_increment)
+    return LaserPoint(
+        index=lateral_index,
+        distance=msg.ranges[lateral_index],
+        angle=lateral_angle,
+    )
 
 
 class RobotMovement(Node):
@@ -73,14 +100,16 @@ class RobotMovement(Node):
             LaserScan, f"/{self.robot_name}/laser_scan", self._scan_callback, 10
         )
 
-    
     def _stop(self, msg: LaserScan) -> bool:
         closest_point = get_closest_point(msg)
         ranges = np.array(msg.ranges)
+
+        if(np.sum(ranges < 999999) > len(ranges)/2):
+            return False
    
         valid_ranges = np.where(ranges < 999999)[0] # Remove points that are too far
+        print(valid_ranges)
 
-        # Condition 1: All points should be consecutive
         for i in range(1, len(valid_ranges)):
             if valid_ranges[i] != valid_ranges[i-1] + 1:
                 return False
@@ -90,29 +119,33 @@ class RobotMovement(Node):
             valid_ranges = valid_ranges[valid_ranges > len(ranges)/2]
         else:
             valid_ranges = valid_ranges[valid_ranges < len(ranges)/2]
+
         if len(valid_ranges) < 2:
             return False
+        
+        print("AYYYYY")
         
         p1 = create_laser_point(valid_ranges[0], msg)
         p2 = create_laser_point(valid_ranges[-1], msg)
 
-        # Condition 2: the extreme points should have approximately the same angle
-        if abs(abs(p1.angle-closest_point.angle) - (abs(p2.angle-closest_point.angle))) > 0.35:
+        p1_relative_angle = abs(p1.angle - closest_point.angle)
+        p2_relative_angle = abs(p2.angle - closest_point.angle)
+
+        print(abs(p1.angle-closest_point.angle)*180/math.pi)
+        print(abs(p2.angle-closest_point.angle)*180/math.pi)
+
+        if p1_relative_angle > 75 * math.pi/180 or p2_relative_angle > 75 * math.pi/180: # Too wide
+            print("A")
+            return False
+
+        if abs(abs(p1.angle-closest_point.angle) - (abs(p2.angle-closest_point.angle))) > 0.75: # Should describe a triangle
+            print("B")
             return False
         
         
         x_distance = abs(p1.x - p2.x)
         print(f"X-Distance: {x_distance:.4f}, w: {abs(x_distance - self.finish_distance):.4f}")
 
-        points = [create_laser_point(i, msg) for i in valid_ranges]
-        x, y = [p.x for p in points], [p.y for p in points]
-        line_fit_error = np.sum((np.polyval(np.polyfit(x, y, 1), x) - y)**2)
-
-        # Condition 3: The points should be approximately in a line (to account for possible errors)
-        if line_fit_error > 0.1:
-            return False
-        
-        # Condition 4: The end wall distance should be approximately the same as self.finish_distance
         return abs(x_distance - self.finish_distance) < self.finish_max_error
 
 
@@ -139,6 +172,43 @@ class RobotMovement(Node):
         )
         
 
+    # def _old_scan_callback(self, msg: LaserScan) -> None:
+    #     closest_point = get_average_point(msg)
+    #     print(f"Closest point ({closest_point})")
+
+    #     lateral_point = get_closest_lateral_point(
+    #         msg, closest_point
+    #     )  # Probably useless?
+    #     print(f"Closest lateral point ({lateral_point})")
+
+    #     closest_lateral_sign = 1 if closest_point.angle > 0 else -1
+        
+    #     if abs(closest_point.angle - math.pi / 2) < 0.1:
+    #         closest_point.angle += 0.1
+
+    #     alpha = (
+    #         abs(closest_point.angle) - math.pi / 2
+    #     )  # This does not need ABS. Don't know why????
+
+    #     target_distance = 0.40
+    #     relative_distance = closest_point.distance - target_distance
+    #     path_sign = np.sign(
+    #         relative_distance
+    #     )  # inside/outside target distance boundary
+    #     steering_sign = closest_lateral_sign * path_sign
+
+    #     new_ang_vel = steering_sign * alpha  # + relative_distance/10
+
+    #     # Print variables
+    #     print(f"Alpha: {rads_to_deg(alpha)}º")
+    #     print(f"Path sign: {path_sign}")
+    #     print(f"Steering sign: {steering_sign}")
+    #     print(
+    #         f"New angular velocity: {rads_to_deg(new_ang_vel)}º/s\n------------------"
+    #     )
+
+    #     k = 2.0
+    #     self.change_vel(1.0, new_ang_vel * k)
 
 
 def main(args=None):
