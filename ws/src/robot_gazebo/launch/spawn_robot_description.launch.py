d86dce3de46957725af3bbec9082df738ac00355
francisco-rente <up201906761@up.pt>
2023-10-16 21:58:55 +0100

    feat: random start in launch configuration

--- ws/src/robot_gazebo/launch/spawn_robot_description.launch.py
+++ ws/src/robot_gazebo/launch/spawn_robot_description.launch.py
@@ -2,11 +2,24 @@ import random
 
 from launch_ros.actions import Node
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 
+def launch_setup(context, *args, **kwargs):
+    random_pose = LaunchConfiguration("random_pose", default="true").perform(context)
 
-def generate_launch_description():
-    position = [0.0, 0.0, 0.2]
-    orientation = [0.0, 0.0, 0.0]
+    print("random_pose: {}, bool: {}".format(random_pose, random_pose == "true"))
+
+    # 8x2
+    x = random.uniform(-1.0, 1.0) if random_pose == "true" else 0.0
+    y = random.uniform(-4.0, 4.0) if random_pose == "true" else 0.0
+
+    y = 1.0 if random_pose == "true" else 0.0
+    z = 0.2
+    position = [x, y, z]
+
+    orientation = [0.0, 0.0, random.uniform(-3.14, 3.14)] if random_pose == "true" else [0.0, 0.0, 0.0]
+
     robot_base_name = "box_bot"
 
     entity_name = robot_base_name  # + "_" + str(random.randint(0, 1000000))
@@ -36,4 +49,15 @@ def generate_launch_description():
         ],
     )
 
-    return LaunchDescription([spawn_robot])
+    return [spawn_robot]
+
+
+def generate_launch_description():
+
+    random_pose_arg = DeclareLaunchArgument(
+            "random_pose", default_value="true",
+            description="Randomize robot pose")
+
+    return LaunchDescription([
+        random_pose_arg,
+        OpaqueFunction(function=launch_setup)])
